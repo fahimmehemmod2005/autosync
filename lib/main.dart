71a30440c8422f0ffd6aef27280b8178322b0e47
MD. FAHIM MEHEMMOD <mdfahimmehemmud@gmail.com>
2025-11-11 18:18:04 +0600

    false banner

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       minTextAdapt: true,
       splitScreenMode: true,
       builder: (_, child) => GetMaterialApp(
-        debugShowCheckedModeBanner: true,
+        debugShowCheckedModeBanner: false,
         title: 'AutoSync',
         initialRoute: appPages.initialRoute,
         getPages: appPages.routes,
