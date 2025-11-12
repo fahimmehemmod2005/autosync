318b318c11d7c63903bff5fb0fd4904bbec6cee0
MD. FAHIM MEHEMMOD <mdfahimmehemmud@gmail.com>
2025-11-12 19:40:32 +0600

    notification page Start

--- lib/app/models/home/bottomNavigatonPages/home.dart
+++ lib/app/models/home/bottomNavigatonPages/home.dart
@@ -148,6 +148,7 @@ class _HomeScreenState extends State<HomeScreen> {
                 ),
               ),
             ),
+            SizedBox(height: 10.h),
             SizedBox(
               height: 258.h,
               child: Card(
